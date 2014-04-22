c1cd1431a369ca90b1423f949854086e56a72d65
davidalpha <david.alpha@gmail.com>
2014-04-22 22:55:42 +0200

    first commit
    tutorial done
    
    doublejump added

---
+++ Assets/playerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	public Vector3 speed = new Vector3 (5f, 0f, 0f);
+	public Vector3 jumpForce = new Vector3 (0f, 5f, 0f);
+	private bool grounded = false;
+	private bool dblJump = true;
+	
+	void FixedUpdate() {
+			if (grounded) {
+						rigidbody.AddForce (speed, ForceMode.Acceleration);
+
+						if (Input.GetButtonDown ("Jump")) {
+								rigidbody.AddForce (jumpForce, ForceMode.VelocityChange);
+						    
+						}
+			}
+
+			if (!grounded & dblJump) {
+								
+				if (Input.GetButtonDown ("Jump")) {
+					rigidbody.AddForce (jumpForce, ForceMode.VelocityChange);
+					dblJump = false;
+				}
+				
+			}
+			
+			if (transform.position.y < -5) {
+			Application.LoadLevel(Application.loadedLevel);
+			grounded = false;
+
+			}
+	 }
+	void OnCollisionEnter(Collision obj) {
+				grounded = true;
+				dblJump = true;
+		}
+	void OnCollisionExit(Collision obj){
+		grounded = false;
+		}
+	void OnTriggerEnter(Collider powerUp){
+		Destroy(powerUp.gameObject);
+		audio.Play();
+	}
+}
+
